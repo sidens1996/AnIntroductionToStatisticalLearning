bf4cbbd43b9883a4b30f9f9d4ef15506f13f52c6
sidens1996 <1424285844@qq.com>
2019-03-18 20:58:39 +0800

    add some files

---
+++ ch2 overview/ch2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 14/03/2019 19:07
+# @Author  : Achilles
+# @Site    : 
+# @File    : ch2 overview.py
+# @Software: PyCharm
+
+# vectors, data, matrices, subsetting
+import numpy as np
+x = np.asarray([1, 2, 3, 4])
+print(x)
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+²Î¿¼×ÊÁÏ
+https://github.com/sujitpal/statlearning-notebooks
+https://lagunita.stanford.edu/courses/HumanitiesSciences/StatLearning/Winter2016/courseware/85b01caa12834b0dbaeff232fb77e123/00b646c1309e42f2b8c1a770516fdef4/?child=first
